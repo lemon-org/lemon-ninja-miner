e9468cd0a3cbdd7b502d3cb4e28e3b37b228411f
AsenaPencheva <asenap86@gmail.com>
2017-05-07 22:45:03 +0300

    added crypto-js

--- public/scripts/controllers/user-controller.js
+++ public/scripts/controllers/user-controller.js
@@ -1,73 +1,139 @@
 import { templateLoader } from 'template-loader';
-import { registerPlayer } from 'data';
-import { loginPlayer } from 'data';
+import * as data from 'data';
+import { validator } from 'validator';
 
 const $root = $('#root');
 
-class UserController {
-    register() {
-        templateLoader.get('register')
-            .then((template) => {
-                $root.html(template);
-            })
-            .then(() => {
-                $('#btn-register').on('click', (ev) => {
-                    let fullName = $('#fullName').val(),
-                        username = $('#username').val(),
-                        password = $('#password').val(),
-                        confirmPassword = $('#verify').val(),
-                        email = $('#email').val();
-
-                    //validations
-
-                    let newUser = {
-                        username,
-                        password,
-                        fullName,
-                        email,
-
-                    };
-
-                    registerPlayer(newUser)
-                        .then((user) => {
-                            //         notifier.success(`${user.username} successfully registered!`);
-                            //         _toggleCLassWhenLoggedIn();
-                            window.location = '#/map';
-                        });
-                    //      .catch((err) => {
-                    //         notifier.error(err);
-                    //         console.log(err);
-                    //     });
-
-                    //   ev.preventDefault();
-                    //      return false;
+export function get() {
+    templateLoader.get('login')
+        .then((template) => {
+            $root.html(template);
+        })
+        .then(() => {
+            $('#login-form-link').click(function (e) {
+                $("#login-form").delay(100).fadeIn(100);
+                $("#register-form").fadeOut(100);
+                $('#register-form-link').removeClass('active');
+                $(this).addClass('active');
+                e.preventDefault();
+                $('#btn-login').on('click', () => {//promise?
+                    login();
                 });
             });
+            $('#register-form-link').click(function (e) {
+                $("#register-form").delay(100).fadeIn(100);
+                $("#login-form").fadeOut(100);
+                $('#login-form-link').removeClass('active');
+                $(this).addClass('active');
+                e.preventDefault();
+                $('#btn-register').on('click', () => { //promise??
+                    register();
+                });
+            });
+            //   return Promise.resolve();
+        });
+}
+
+export function register() {
+    let $username = $('#username'),
+        $password = $('#password'),
+        $confirmPassword = $('#confirm-password'),
+        $email = $('#email'),
+
+        username = $username.val(),
+        password = $password.val(),
+        confirmPassword = $confirmPassword.val(),
+        email = $email.val();
+
+    if (!validator.validateEmptyInput(username)) {
+        toastr.error('Username is required!');
+        $password.val('');
+        $confirmPassword.val('');
     }
 
-    login() {
-        templateLoader.get('login')
-            .then((template) => {
-                $root.html(templateH);
-            })
-            .then(() => {
-                $('#btn-login').on('click', (ev) => {
+    if (!validator.validateEmptyInput(password)) {
+        toastr.error('Password is required!');
+        $confirmPassword.val('');
+    }
 
-                    let username = $('#userName-log').val(),
-                        password = $('#password-log').val();
+    if (!validator.validateEmptyInput(confirmPassword)) {
+        toastr.error('Confir password is required!');
+        $password.val('');
+    }
 
-                    let user = {
-                        username,
-                        password
-                    };
-                    loginPlayer(user)
-                        .then((user) => {
-                            window.location = '#/map';
-                        });
-                });
-            }
+    if (!validator.validateEmptyInput(email)) {
+        toastr.error('E-mail is required!');
+        $password.val('');
+        $confirmPassword.val('');
+    }
 
+    if (!validator.validateUserName(username)) {
+        toastr.error('Username must be between 5 and 15 symbols!');
+        $username.val('');
+        $password.val('');
+        $confirmPassword.val('');
+    }
+
+    if (!validator.validatePassword(password)) {
+        toastr.error('Password must be between 5 and 20 symbols!');
+        $password.val('');
+        $confirmPassword.val('');
+    }
+
+    if (validator.validateEmail(email)) {
+        toastr.error('E-mail must be valid e-mail address!');
+        $password.val('');
+        $confirmPassword.val('');
+        $email.val('');
+    }
+
+    if (password !== confirmPassword) {
+        toastr.error('Password doesn\'t match'); // drug tekst
+        $password.val('');
+        $confirmPassword.val('');
+    }
+
+    const passHash = CryptoJS.SHA1(username + password).toString();
+    data.registerUser(username, passHash, email)
+        .then((user) => {
+            toastr.success(`New user ${user.username} registered!`);
+            window.location = '#/map';
+        })
+        .catch((err) => {
+            toastr.error(`${err} occured`);
+            console.log(err);
+        });
+};
+
+export function login() {
+    const username = $('#username').val(),
+        password = $('#password').val(),
+        passHash = CryptoJS.SHA1(username + password).toString();
+
+    if (!validator.validateUserName(username)) {//ima validacii na bootstrap no dali mogat da se polzvat
+        $username.val('');
+        $password.val('');
+    }
+
+    if (!validator.validatePassword(password)) {
+        $password.val('');
+    }
+
+    data.login(username, passHash)
+        .then((user) => {
+            toastr.success(`User ${user.username} logged in!`);
+            window.location = '#/map';
+        })
+        .catch((err) => {
+            toastr.error(`${err} occured`);
+            console.log(err);
+        });
 }
-    const userController = new UserController();
 
-export {userController};
+export function logout() {
+    data.logoutUser()
+        .then(() => {
+            toastr.success(`User logged out!`);
+            location.href = '';
+        })
+}
---
+++ public/scripts/utils/data.js
@@ -0,0 +1,83 @@
+import { requester } from 'requester';
+
+
+const LOCAL_STORAGE_USERNAME_KEY = 'signed-in-user-username',
+    LOCAL_STORAGE_AUTHKEY_KEY = 'signed-in-user-auth-key';
+
+export function getPuzzle(id) { // triabva da vzima puzelite ot player
+
+    return requester.get('api/puzzles' + id)
+        .then(function (res) {
+            const puzzle = res.result;
+            return puzzle;
+        });
+}
+
+export function getPuzzles() {
+
+    return requester.get('api/puzzles')
+        .then(function (res) {
+            const puzzles = res.result;
+            return puzzles;
+        });
+}
+
+export function getReachedLevel() {
+    const options = {
+        headers: {
+            'x-auth-key': localStorage.getItem(LOCAL_STORAGE_AUTHKEY_KEY)
+        }
+    };
+    return requester.get('api/users/reachedLevel', options)
+        .then(function (res) {
+            const reachedLevel = res.result;
+            return reachedLevel;
+        });
+}
+
+export function registerUser(username, passHash, email) {
+    const body = {
+        username,
+        passHash,
+        email,
+        reachedLevel: 0
+    };
+    return requester.post('api/users', {
+        data: body
+    })
+        .then(function (res) {
+            const user = res.result;
+            localStorage.setItem(LOCAL_STORAGE_USERNAME_KEY, user.username);
+            localStorage.setItem(LOCAL_STORAGE_AUTHKEY_KEY, user.authKey); //puzzles??
+            return user;
+        });
+}
+
+export function login(user, passHash) {
+    const body = {
+        username,
+        passHash
+    };
+    return requester.put('/api/users/auth', {
+        data: body
+    })
+        .then(function (res) {
+            const user = res.result;
+            localStorage.setItem(LOCAL_STORAGE_USERNAME_KEY, user.username);
+            localStorage.setItem(LOCAL_STORAGE_AUTHKEY_KEY, user.authKey);
+            return user;
+        });
+}
+
+export function logoutUser() {
+    var promise = new Promise(function (resolve, reject) {
+        localStorage.removeItem(LOCAL_STORAGE_USERNAME_KEY);
+        localStorage.removeItem(LOCAL_STORAGE_AUTHKEY_KEY);
+        resolve();
+    });
+    return promise;
+}
+
+export function isLogged() {
+    return localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY) != null;
+}
