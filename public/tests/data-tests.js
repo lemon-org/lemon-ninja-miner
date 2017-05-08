fd8d4c8ed41dfc5094324fe6908ebaa3ead7677e
Levent Dzhinali <Levent Dzhinali>
2017-05-08 17:47:10 +0300

    Added data test files

---
+++ public/tests/data-tests.js
@@ -0,0 +1,20 @@
+describe('Data layer tests', () => {
+           
+    it('Test test', () =>{
+        expect(2 + 2).to.equal(4);
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+});
