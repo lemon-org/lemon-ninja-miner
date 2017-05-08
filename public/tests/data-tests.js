564be07cf2dd1789ae9ac37f83c62160cde34cf4
Levent Dzhinali <Levent Dzhinali>
2017-05-09 00:50:33 +0300

    all

--- public/tests/data-tests.js
+++ public/tests/data-tests.js
@@ -1,19 +1,35 @@
-import { requester } from 'requester';
+import requester from 'requester';
+import * as data from 'data';
 
-describe('Data layer tests', () => {   
-    describe('RegisterUser tests', (done) => {
+describe('Data layer tests', () => {  
+    describe('RegisterUser tests', () => {   
+        it('Expect register to make POST', (done) => {
+       console.log(1);
             const username = 'testuser';
-            const passHash = CryptoJS.SHA1(username + '12341234').toString();
-            const email = 'test@email.com'
+            const passHash = '12341234';
+            const email = 'test@email.com';
 
-            sinon.stub(requester, 'post')
-                .returns(new Promise());
+            const response = {
+                result: {
+                    username: username,
+                    authKey: ''
+                }
+            }
+
+            const requesterStub = sinon.stub(requester, 'post')
+                .returns(Promise.resolve(response));
+
+            //sinon.stub(localStorage, 'setItem');
 
             data.registerUser(username, passHash, email)
                 .then(() => {
-                    expect(requester.post).to.be.calledOnce();
-                });
+                    console.log(requesterStub.callCount);
+                    expect(requesterStub).to.be.calledOnce;
+                })
+                .then(done, done);
 
-            requester.post.restore();
+            requesterStub.post.restore();
+            //localStorage.setItem.restore();
+        });   
     });
 });
