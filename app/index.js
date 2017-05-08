e0a2ab679eba51ffac4417619de456b297dfca28
Levent Dzhinali <Levent Dzhinali>
2017-05-08 22:13:25 +0300

    Added system.config.js, tests.html, changes to index.html

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./public'));
-app.use(express.static('./node_modules'))
+app.use(express.static('./node_modules'));
 
 app.listen(process.env.PORT || 3333, function () {
     console.log(`Server is running at ${process.env.PORT || 3333}`);
--- public/tests/data-tests.js
+++ public/tests/data-tests.js
@@ -1,20 +1,19 @@
-describe('Data layer tests', () => {
-           
-    it('Test test', () =>{
-        expect(2 + 2).to.equal(4);
-    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+import { requester } from 'requester';
 
+describe('Data layer tests', () => {   
+    describe('RegisterUser tests', (done) => {
+            const username = 'testuser';
+            const passHash = CryptoJS.SHA1(username + '12341234').toString();
+            const email = 'test@email.com'
+
+            sinon.stub(requester, 'post')
+                .returns(new Promise());
+
+            data.registerUser(username, passHash, email)
+                .then(() => {
+                    expect(requester.post).to.be.calledOnce();
+                });
+
+            requester.post.restore();
+    });
 });
