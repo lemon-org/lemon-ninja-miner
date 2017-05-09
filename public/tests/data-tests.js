84e363da7a56c7b4421de89b27ed16ea829c2299
Levent Dzhinali <Levent Dzhinali>
2017-05-09 13:45:27 +0300

    Login tests

--- public/tests/data-tests.js
+++ public/tests/data-tests.js
@@ -8,8 +8,6 @@ describe('Data layer tests', () => {
     describe('RegisterUser tests', () => {
 
         let requesterPostStub;
-        //let cryptoJSStub;
-        
         let username = 'testuser';
         let password = '12341234';
         let passHash = username + password;
@@ -24,18 +22,13 @@ describe('Data layer tests', () => {
 
         beforeEach(() => {
             requesterPostStub = sinon.stub(requester, 'post');
-           /* cryptoJSStub = sinon.stub(CryptoJS, 'SHA1')
-                .returns(passHash);*/
+            requesterPostStub.returns(Promise.resolve(response));
         });
         afterEach(() => {
             requesterPostStub.restore();
-            //cryptoJSStub.restore();
         });
 
         it('Expect registerUser to make a POST request', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
                 .then(() => {
                     expect(requesterPostStub).to.be.calledOnce;
@@ -44,9 +37,6 @@ describe('Data layer tests', () => {
         });
 
         it('Expect registerUser to make a POST request to api/users/register ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
                 .then(() => {
                     expect(requesterPostStub).to.be.calledWith('api/users/register');
@@ -55,9 +45,6 @@ describe('Data layer tests', () => {
         });
 
         it('Expect registerUser to make a POST request with username ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
             .then(() => {
                 expect(requesterPostStub.args[0][1].data.username).to.be.equal(response.dbUser.username);
@@ -65,21 +52,7 @@ describe('Data layer tests', () => {
             .then(done, done);
         });
 
-        /*it('Expect registerUser to make a call to CryptoJS.SHA1() ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
-            data.registerUser(username, passHash, email)
-            .then(() => {
-                expect(cryptoJSStub).to.have.been.calledOnce;
-            })  
-            .then(done, done);
-        });*/
-
         it('Expect registerUser to make a POST request with passHash ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
             .then(() => {
                 expect(requesterPostStub.args[0][1].data.passHash).to.be.equal(response.dbUser.passHash);
@@ -88,9 +61,6 @@ describe('Data layer tests', () => {
         });
 
         it('Expect registerUser to set username in localStorage ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
             .then(() => {
                 expect(localStorage.getItem('signed-in-user-username')).to.be.equal(response.dbUser.username);
@@ -99,9 +69,6 @@ describe('Data layer tests', () => {
         });
 
         it('Expect registerUser to set _id in localStorage ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
             .then(() => {
                 expect(localStorage.getItem('signed-in-user-auth-key')).to.be.equal(response.dbUser._id);
@@ -110,18 +77,12 @@ describe('Data layer tests', () => {
         });
 
         it('Expect registerUser to return a Promise ', () => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             const promise =  data.registerUser(username, passHash, email)
           
             expect(promise).to.be.a.instanceof(Promise);
         });
 
         it('Expect registerUser to return a Promise with correct username ', (done) => {
-
-            requesterPostStub.returns(Promise.resolve(response));
-
             data.registerUser(username, passHash, email)
             .then((value) => {
                 expect(value).to.be.deep.equal(response.dbUser);
@@ -130,8 +91,95 @@ describe('Data layer tests', () => {
         });
     });
 
-    
 
+
+
+
+    describe('Login tests', () => {
+
+        let requesterPutStub;
+        let username = 'testuser';
+        let password = '12341234';
+        let passHash = username + password;
+        let email = 'test@email.com';
+
+        const response = {
+            user: {
+                username: username,
+                authKey: 'SOME_KEY'
+            }
+        }
+
+        beforeEach(() => {
+            requesterPutStub = sinon.stub(requester, 'put');
+            requesterPutStub.returns(Promise.resolve(response));
+        });
+        afterEach(() => {
+            requesterPutStub.restore();
+        });
+
+        it('Expect login to make a PUT request', (done) => {
+            data.login(username, passHash)
+                .then(() => {
+                    expect(requesterPutStub).to.be.calledOnce;
+                })
+                .then(done, done);
+        });
+
+        it('Expect login to make a PUT request to api/users/login ', (done) => {
+            data.login(username, passHash)
+                .then(() => {
+                    expect(requesterPutStub).to.be.calledWith('api/users/login');
+                })
+                .then(done, done);
+        });
+
+        it('Expect login to make a PUT request with username ', (done) => {
+            data.login(username, passHash)
+            .then(() => {
+                expect(requesterPutStub.args[0][1].data.username).to.be.equal(response.user.username);
+            })  
+            .then(done, done);
+        });
+
+        it('Expect loign to make a PUT request with passHash ', (done) => {
+            data.login(username, passHash)
+            .then(() => {
+                expect(requesterPutStub.args[0][1].data.passHash).to.be.equal(response.user.passHash);
+            })  
+            .then(done, done);
+        });
+
+        it('Expect login to set username in localStorage ', (done) => {
+            data.login(username, passHash)
+            .then(() => {
+                expect(localStorage.getItem('signed-in-user-username')).to.be.equal(response.user.username);
+            })  
+            .then(done, done);
+        });
+
+        it('Expect login to set _id in localStorage ', (done) => {
+            data.login(username, passHash)
+            .then(() => {
+                expect(localStorage.getItem('signed-in-user-auth-key')).to.be.equal(response.user.authKey);
+            })  
+            .then(done, done);
+        });
+
+        it('Expect login to return a Promise ', () => {
+            const promise =  data.login(username, passHash)
+          
+            expect(promise).to.be.a.instanceof(Promise);
+        });
+
+        it('Expect login to return a Promise with correct username ', (done) => {
+            data.login(username, passHash)
+            .then((value) => {
+                expect(value).to.be.deep.equal(response.user);
+            })  
+            .then(done, done);
+        });
+    });
 
 
 });
