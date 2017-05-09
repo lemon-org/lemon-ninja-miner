6374513185d4881c4821a0e2f6ccfed4f3a7b33d
AsenaPencheva <asenap86@gmail.com>
2017-05-09 15:55:53 +0300

    fix bug in timer

--- public/scripts/controllers/puzzle-controller.js
+++ public/scripts/controllers/puzzle-controller.js
@@ -69,8 +69,8 @@ export function get(params) {
             }
 
             $('.cell').on('click', function () {
-                  const minesCount = (puzzle.size * puzzle.size / 2 | 0) + 1 - puzzle.difficultness,
-emptyCellsCount = puzzle.size * puzzle.size - minesCount;
+                const minesCount = (puzzle.size * puzzle.size / 2 | 0) + 1 - puzzle.difficultness,
+                    emptyCellsCount = puzzle.size * puzzle.size - minesCount;
                 const col = $(this).parent().children().index($(this));
                 const row = $(this).parent().parent().children().index($(this).parent());
                 $(this).removeClass('close');
@@ -116,5 +116,9 @@ emptyCellsCount = puzzle.size * puzzle.size - minesCount;
                     $(this).toggleClass('mark');
                 }
             });
+
+            $('#quit-game').on('click', function () {
+                clearInterval(pointsFunc);
+            })
         });
 }
